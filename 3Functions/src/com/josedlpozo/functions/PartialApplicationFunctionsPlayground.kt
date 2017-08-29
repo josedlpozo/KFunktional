5ba0573ec83db02a30ff25f413557e86bcdec5a1
Jose del Pozo <jmdelpozo@idealista.com>
2017-08-29 12:26:16 +0200

    Remove invoke methods

--- 3Functions/src/com/josedlpozo/functions/PartialApplicationFunctionsPlayground.kt
+++ 3Functions/src/com/josedlpozo/functions/PartialApplicationFunctionsPlayground.kt
@@ -6,16 +6,16 @@ fun logger(level: String) : (String) -> Unit = { print("[$level] $it") }
 
 fun operation(a: Int, b: Int, logger: (String) -> (Unit)) {
     if (a < 0) {
-        logger.invoke("a must be greater than zero")
+        logger("a must be greater than zero")
         return
     }
 
     if (b < 0) {
-        logger.invoke("b must be greater than zero")
+        logger("b must be greater than zero")
         return
     }
 
-    logger.invoke((a + b).toString())
+    logger((a + b).toString())
 }
 
 fun main(args: Array<String>) {
